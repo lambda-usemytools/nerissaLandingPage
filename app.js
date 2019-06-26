869e4d173a81505e95ddb09c0d97efb915017622
Nerissa Taylor <taylornerissa@hotmail.com>
2019-06-26 17:41:20 -0400

    tab made on about page

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
 const joinIn = document.querySelector('.join-today').onclick = function () {
   location.href = "https://gracious-ptolemy-4a8ec7.netlify.com/";
 };
+
+const createAcct = document.querySelector('.create-account');
+createAcct.addEventListener('mouseover',() =>{
+  alert('Don\'t forget to sign up for our weekly newletter!')
+});
+
+
+
+
+
+class TabTag {
+  
+  constructor(tag) {
+     
+    
+    this.tabPage = document.querySelector(`.page[data-tab='${tag.dataset.tab}']`);
+    
+    
+    this.tabPage = new Page(this.tabPage);
+    
+    
+    
+    tag.addEventListener('click', () => this.tagClick() );
+  }
+  
+  
+  tagClick() {
+    this.tabPage.togglePage();
+  }
+}
+
+
+class Page {
+  constructor(site) {
+    
+    
+    this.site = site;
+  }
+  
+  togglePage() {
+    this.site.classList.toggle('change');
+  }
+}
+
+
+const tags = document.querySelectorAll('.aboutLink');
+tags.forEach(tag => new TabTag(tag));
+
+
